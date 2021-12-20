f5e62052d79c1aabbad37df42ce34bd09f0ba345
jaiproud <JaiProud@yandex.ru>
2021-12-20 11:14:16 +0400

    add login check in Registration Form

--- RegistrationForm.cs
+++ RegistrationForm.cs
@@ -22,25 +22,53 @@ namespace LogisticProgram
         {
             var id = Guid.NewGuid().ToString();
             string connectString = "server=localhost;user id=root;database=persons;persistsecurityinfo=True;password=root";
-            string insertDataQuery = "INSERT INTO persons (pName,pSureName,pLogin,pPassword,pPersonID) values ('" + this.nameTextBox.Text+"'," +
-                "'"+this.surenameTextBox.Text + "','"+this.loginTextBox.Text + "','"+this.passwordTextBox.Text + "','" + id + "')";
-            MySqlConnection conn = new MySqlConnection(connectString);
-            MySqlCommand cmd = new MySqlCommand(insertDataQuery, conn);
-            MySqlDataReader dataReader;
+            string loginQuery = "SELECT * FROM persons WHERE pLogin= '" + this.loginTextBox.Text + "'";
+            MySqlConnection conn1 = new MySqlConnection(connectString);
+            MySqlCommand cmd1 = new MySqlCommand(loginQuery, conn1);
+            MySqlDataReader reader;
+            int count = 0;
             try
             {
-                conn.Open();
-                dataReader = cmd.ExecuteReader();
-                MessageBox.Show("Person Register Successfully");
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("Unable to Insert the Data due to" + ex);
-            }
+                conn1.Open();
+                reader = cmd1.ExecuteReader();
+                while (reader.Read())
+                {
+                    count += 1;
+                }
+                conn1.Close();
+                if (count >= 1)
+                {
+                    MessageBox.Show("Этот логин уже есть, придумайте новый");
+                }
+                else
+                {
+                    string insertDataQuery = "INSERT INTO persons (pName,pSureName,pLogin,pPassword,pPersonID) values ('" + this.nameTextBox.Text + "'," +
+                "'" + this.surenameTextBox.Text + "','" + this.loginTextBox.Text + "','" + this.passwordTextBox.Text + "','" + id + "')";
+                    MySqlConnection conn = new MySqlConnection(connectString);
+                    MySqlCommand cmd = new MySqlCommand(insertDataQuery, conn);
+                    MySqlDataReader dataReader;
+                    try
+                    {
+                        conn.Open();
+                        dataReader = cmd.ExecuteReader();
+                        conn.Close();
+                        MessageBox.Show("Person Register Successfully");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Unable to Insert the Data due to" + ex);
+                    }
 
-            LoginForm lform = new LoginForm();
-            lform.Show();
-            this.Hide();
+                    LoginForm lform = new LoginForm();
+                    lform.Show();
+                    this.Hide();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to Get the Values due to" + ex);
+            }
+            
         }
     }
 }

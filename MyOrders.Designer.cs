110447a0b8bfa957f0f2b21125445c8f8341849e
jaiproud <JaiProud@yandex.ru>
2021-12-20 11:04:16 +0400

    add My Orders

---
+++ MyOrders.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace LogisticProgram
+{
+    partial class MyOrders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myOrdersButton = new System.Windows.Forms.Button();
+            this.toCreateOrderForm = new System.Windows.Forms.Button();
+            this.refreshOrders = new System.Windows.Forms.Button();
+            this.myOrdersTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // myOrdersButton
+            // 
+            this.myOrdersButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.myOrdersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.myOrdersButton.Location = new System.Drawing.Point(12, 58);
+            this.myOrdersButton.Name = "myOrdersButton";
+            this.myOrdersButton.Size = new System.Drawing.Size(187, 40);
+            this.myOrdersButton.TabIndex = 4;
+            this.myOrdersButton.Text = "Мои заказы";
+            this.myOrdersButton.UseVisualStyleBackColor = false;
+            // 
+            // toCreateOrderForm
+            // 
+            this.toCreateOrderForm.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.toCreateOrderForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toCreateOrderForm.Location = new System.Drawing.Point(12, 12);
+            this.toCreateOrderForm.Name = "toCreateOrderForm";
+            this.toCreateOrderForm.Size = new System.Drawing.Size(187, 40);
+            this.toCreateOrderForm.TabIndex = 3;
+            this.toCreateOrderForm.Text = "Создать заказ";
+            this.toCreateOrderForm.UseVisualStyleBackColor = false;
+            this.toCreateOrderForm.Click += new System.EventHandler(this.toCreateOrderForm_Click);
+            // 
+            // refreshOrders
+            // 
+            this.refreshOrders.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.refreshOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.refreshOrders.Location = new System.Drawing.Point(12, 104);
+            this.refreshOrders.Name = "refreshOrders";
+            this.refreshOrders.Size = new System.Drawing.Size(187, 40);
+            this.refreshOrders.TabIndex = 6;
+            this.refreshOrders.Text = "Обновить ленту";
+            this.refreshOrders.UseVisualStyleBackColor = false;
+            this.refreshOrders.Click += new System.EventHandler(this.refreshOrders_Click);
+            // 
+            // myOrdersTextBox
+            // 
+            this.myOrdersTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.myOrdersTextBox.Location = new System.Drawing.Point(280, 12);
+            this.myOrdersTextBox.Multiline = true;
+            this.myOrdersTextBox.Name = "myOrdersTextBox";
+            this.myOrdersTextBox.ReadOnly = true;
+            this.myOrdersTextBox.Size = new System.Drawing.Size(202, 35);
+            this.myOrdersTextBox.TabIndex = 0;
+            this.myOrdersTextBox.Text = "Ваши заказы:";
+            this.myOrdersTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MyOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 600);
+            this.Controls.Add(this.myOrdersTextBox);
+            this.Controls.Add(this.refreshOrders);
+            this.Controls.Add(this.myOrdersButton);
+            this.Controls.Add(this.toCreateOrderForm);
+            this.Name = "MyOrders";
+            this.Text = "MyOrders";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button myOrdersButton;
+        private System.Windows.Forms.Button toCreateOrderForm;
+        private System.Windows.Forms.Button refreshOrders;
+        private System.Windows.Forms.TextBox myOrdersTextBox;
+    }
+}
---
+++ MyOrders.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogisticProgram
+{
+    public partial class MyOrders : Form
+    {
+
+        public MyOrders()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void toCreateOrderForm_Click(object sender, EventArgs e)
+        {
+            CreateOrderForm cform = new CreateOrderForm();
+            cform.Show();
+            this.Hide();
+        }
+
+        private void refreshOrders_Click(object sender, EventArgs e)
+        {
+            string conString = "server=localhost;user id=root;database=persons;persistsecurityinfo=True;password=root";
+            string orderQuery = "SELECT * FROM orders WHERE oPersonID= '" + LoginForm.id + "'";
+            MySqlConnection conn = new MySqlConnection(conString);
+            MySqlCommand cmd = new MySqlCommand(orderQuery, conn);
+            MySqlDataReader reader;
+            try
+            {
+                conn.Open();
+                reader = cmd.ExecuteReader();
+                TextBox oldbbox = myOrdersTextBox;
+                List<string> info = new List<string>();
+                while (reader.Read())
+                {
+                    TextBox newOrder = new TextBox();
+                    conn.Open();
+                    string bufer = cmd.ExecuteScalar().ToString();
+                    conn.Close();
+                    newOrder.Text = bufer;
+                    newOrder.ReadOnly = true;
+                    newOrder.Width = 600;
+                    newOrder.Height = 30;
+                    newOrder.Location = new Point(oldbbox.Location.X, oldbbox.Location.Y + oldbbox.Height + 10);
+                    this.Controls.Add(newOrder);
+                    oldbbox = newOrder;
+                }
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to Get the Values due to" + ex);
+            }
+
+            
+        }
+    }
+}

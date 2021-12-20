6e535f4683f4f40450c50ce335e2b1e390b30fb1
jaiproud <JaiProud@yandex.ru>
2021-12-20 11:02:56 +0400

    add Create Order Form

---
+++ CreateOrderForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace LogisticProgram
+{
+    partial class CreateOrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.heigthTextBox = new System.Windows.Forms.TextBox();
+            this.lengthTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.weightTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.createOrderButton = new System.Windows.Forms.Button();
+            this.myOrdersButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.Controls.Add(this.createOrderButton);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.nameTextBox);
+            this.panel1.Controls.Add(this.widthTextBox);
+            this.panel1.Controls.Add(this.heigthTextBox);
+            this.panel1.Controls.Add(this.lengthTextBox);
+            this.panel1.Controls.Add(this.addressTextBox);
+            this.panel1.Controls.Add(this.weightTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(205, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 576);
+            this.panel1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(599, 366);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Высота";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(335, 366);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ширина";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(85, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Длина";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(20, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(715, 40);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Укажите длину (не более 80 см), ширину (не более 80 см), высоту (не более 60 см)." +
+    "\r\n\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(16, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Напишите ваш адрес:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Укажите вес товара (в килограммах):";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(20, 98);
+            this.nameTextBox.Multiline = true;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(715, 31);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(276, 389);
+            this.widthTextBox.Multiline = true;
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(196, 31);
+            this.widthTextBox.TabIndex = 6;
+            // 
+            // heigthTextBox
+            // 
+            this.heigthTextBox.Location = new System.Drawing.Point(539, 389);
+            this.heigthTextBox.Multiline = true;
+            this.heigthTextBox.Name = "heigthTextBox";
+            this.heigthTextBox.Size = new System.Drawing.Size(196, 31);
+            this.heigthTextBox.TabIndex = 5;
+            // 
+            // lengthTextBox
+            // 
+            this.lengthTextBox.Location = new System.Drawing.Point(24, 389);
+            this.lengthTextBox.Multiline = true;
+            this.lengthTextBox.Name = "lengthTextBox";
+            this.lengthTextBox.Size = new System.Drawing.Size(196, 31);
+            this.lengthTextBox.TabIndex = 4;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(20, 269);
+            this.addressTextBox.Multiline = true;
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(715, 31);
+            this.addressTextBox.TabIndex = 3;
+            // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Location = new System.Drawing.Point(20, 180);
+            this.weightTextBox.Multiline = true;
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(715, 31);
+            this.weightTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите название товара:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Создать заказ";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // createOrderButton
+            // 
+            this.createOrderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.createOrderButton.Location = new System.Drawing.Point(260, 466);
+            this.createOrderButton.Name = "createOrderButton";
+            this.createOrderButton.Size = new System.Drawing.Size(227, 40);
+            this.createOrderButton.TabIndex = 13;
+            this.createOrderButton.Text = "Сделать заказ";
+            this.createOrderButton.UseVisualStyleBackColor = true;
+            this.createOrderButton.Click += new System.EventHandler(this.createOrderButton_Click);
+            // 
+            // myOrdersButton
+            // 
+            this.myOrdersButton.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.myOrdersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.myOrdersButton.Location = new System.Drawing.Point(12, 58);
+            this.myOrdersButton.Name = "myOrdersButton";
+            this.myOrdersButton.Size = new System.Drawing.Size(187, 40);
+            this.myOrdersButton.TabIndex = 2;
+            this.myOrdersButton.Text = "Мои заказы";
+            this.myOrdersButton.UseVisualStyleBackColor = false;
+            this.myOrdersButton.Click += new System.EventHandler(this.myOrdersButton_Click);
+            // 
+            // CreateOrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 600);
+            this.Controls.Add(this.myOrdersButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Name = "CreateOrderForm";
+            this.Text = "CreateOrderForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox heigthTextBox;
+        private System.Windows.Forms.TextBox lengthTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox weightTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button createOrderButton;
+        private System.Windows.Forms.Button myOrdersButton;
+    }
+}
---
+++ CreateOrderForm.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogisticProgram
+{
+    public partial class CreateOrderForm : Form
+    {
+        public CreateOrderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void createOrderButton_Click(object sender, EventArgs e)
+        {
+            if ((int.Parse(lengthTextBox.Text) > 80) || (int.Parse(widthTextBox.Text) > 80) || (int.Parse(heigthTextBox.Text) > 60))
+            {
+                MessageBox.Show("Мы не можем принять заказ, из-за несоответствия требованиям.");
+            }
+
+            else
+            {
+                double weigth = 200 * double.Parse(weightTextBox.Text);
+                MessageBox.Show($"Стоимость вашей доставки: {Math.Round(weigth, 2)} рублей.");
+
+                string connectString = "server=localhost;user id=root;database=persons;persistsecurityinfo=True;password=root";
+                string insertDataQuery = "INSERT INTO orders (oName,oWeigth,oAddress,oLength,oWidth,oHeigth,oCost,oPersonID) values ('" + this.nameTextBox.Text + "'," +
+                    "'" + this.weightTextBox.Text + "','" + this.addressTextBox.Text + "','" + this.lengthTextBox.Text + "','" + this.widthTextBox.Text + "'," +
+                    "'" + this.heigthTextBox.Text + "','" + Math.Round(weigth, 2) + "','" + LoginForm.id + "')";
+                MySqlConnection conn = new MySqlConnection(connectString);
+                MySqlCommand cmd = new MySqlCommand(insertDataQuery, conn);
+                MySqlDataReader dataReader;
+                try
+                {
+                    conn.Open();
+                    dataReader = cmd.ExecuteReader();
+                    MessageBox.Show("The order has been successfully placed.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("The order was not created." + ex);
+                }
+                nameTextBox.Text = null; weightTextBox.Text = null; addressTextBox.Text = null; 
+                lengthTextBox.Text = null; widthTextBox.Text = null; heigthTextBox.Text = null;
+
+            }
+        }
+
+        private void myOrdersButton_Click(object sender, EventArgs e)
+        {
+            MyOrders mform = new MyOrders();
+            mform.Show();
+            this.Hide();
+        }
+    }
+}
